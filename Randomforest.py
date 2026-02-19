a537908db264c62dfa0bf61ed95dd59be9dc03bc
nishchalabr <nishchalabr87@gmail.com>
2026-02-19 11:58:20 +0530

    Update Randomforest.py

--- Randomforest.py
+++ Randomforest.py
@@ -2,7 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+
+# Example dataset (marks vs salary/score/etc.)
+X = np.array([[10],[20],[30],[40],[50],[60],[70]])
+y = np.array([100,200,300,400,500,600,700])
+
+# Create model
 RandomForestRegModel = RandomForestRegressor()
-RandomForestRegModel.fit(X,y)
-X_marks=[[70]]
+
+# Train model
+RandomForestRegModel.fit(X, y)
+
+# Predict for 70 marks
+X_marks = np.array([[70]])
 print(RandomForestRegModel.predict(X_marks))
